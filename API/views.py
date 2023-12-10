5b1b3fd6295ef26e5934902a35a11936301421eb
AHZ1386 <100196498+AHZ1386@users.noreply.github.com>
2023-12-10 23:24:33 +0330

    update and delet test

--- API/views.py
+++ API/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from . serializers import UserProfileSerializer,CreateUserProfileSerializer
 from rest_framework.response import Response
 from .models import UserProfile
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 @api_view(['GET','POST'])
 def  test(request):
@@ -25,3 +26,24 @@ def  test(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+
+@api_view(["PUT","DELETE"])
+def  update_test(request,id):
+    
+    if request.mothed =='PUT':
+        user_profile = UserProfile.objects.get(id=id)
+        serializer = UserProfileSerializer(
+        instance = user_profile,
+        data = request.data
+        )
+        serializer.is_valid(raise_exception=True)
+        
+        serializer.save()
+
+        return Response(serializer.data,status=200)
+@api_view(["DELETE"])
+def delete_test(reqeust,id):
+    user_profile = UserProfile.objects.get(id=id)
+    user_profile.delete()
+    return Response(status=HTTP_204_NO_CONTENT)
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from API.views import test
+from API.views import test,update_test,delete_test
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("Api/", test)
+    path("user/", test),
+    path('update-user/<int:id>/',update_test),
+    path('delete-user/<int:id>/',delete_test)
 ]
