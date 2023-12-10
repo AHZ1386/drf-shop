44bbac71663b44b6b2f0736a1ebdd056c2d3048a
AHZ1386 <100196498+AHZ1386@users.noreply.github.com>
2023-12-10 22:37:03 +0330

    testing Post method

--- API/serializers.py
+++ API/serializers.py
@@ -1,7 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import UserProfile
 
-class UserProfileSerializer(ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+    
+
+class CreateUserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['name','age','bio','email']
+        
--- API/views.py
+++ API/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from . serializers import UserProfileSerializer
+from . serializers import UserProfileSerializer,CreateUserProfileSerializer
 from rest_framework.response import Response
 from .models import UserProfile
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def  test(request):
-    data = UserProfile.objects.all()
-    serializer_data = UserProfileSerializer(data,many=True)
-    
-    return Response(serializer_data.data)
+    if request.method == 'GET':
+        data = UserProfile.objects.all()
+        serializer_data = UserProfileSerializer(data,many=True)
+        
+        return Response(serializer_data.data)
+    if request.method == 'POST':
+        request_data = request.data
+        
+        serializer = CreateUserProfileSerializer(data=request_data)
+        
+        
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data)
+        # else:
+        #     return Response(serializer.errors,status=401)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
